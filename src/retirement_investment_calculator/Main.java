30d8333d6b15355f66c5866a410e230a2eae8a7d
UnityUser525 <michaelmihailov@hotmail.com>
2026-02-04 09:23:26 -0600

    Refactored into helper functions
    moved code into required helper functions

--- src/retirement_investment_calculator/Main.java
+++ src/retirement_investment_calculator/Main.java
@@ -193,5 +193,97 @@ public class Main {
 		// logic
 		
 	}
-
+	
+	public static int readIntInRange(Scanner sc, String prompt, int min, int max)
+	{
+		int res = -1;
+		boolean done = false;
+		
+		while (!done) // get input until correct
+		{
+			System.out.print(prompt);
+			
+			try
+			{
+				res = Integer.parseInt(sc.nextLine());
+			}
+			catch (NumberFormatException e)
+			{
+				System.out.println("ERROR! Input must be an integer!");
+				continue;
+			}
+			
+			if (res < min || res > max)
+			{
+				System.out.println("ERROR! Input must be between " + min + " and " + max);
+				continue;
+			}
+			
+			done = true;
+		}
+		
+		return res;
+	}
+	
+	public static double readDoubleInRange(Scanner sc, String prompt, double min, double max)
+	{
+		double res = -1.0;
+		boolean done = false;
+		
+		while (!done) // get input until correct
+		{
+			System.out.print(prompt);
+			
+			try
+			{
+				res = Double.parseDouble(sc.nextLine());
+			}
+			catch (NumberFormatException e)
+			{
+				System.out.println("ERROR! Input must be an decimal!");
+				continue;
+			}
+			
+			if (res < min || res > max)
+			{
+				System.out.println("ERROR! Input must be between " + min + " and " + max);
+				continue;
+			}
+			
+			done = true;
+		}
+		
+		return res;
+	}
+	
+	public static int readCompoundingChoice(Scanner sc)
+	{
+		int res = -1;
+		
+		do // get compoundingFrequency
+		{
+			System.out.println("1: Annualy\n2: Monthly\n3:Daily");
+			System.out.print("Compounding Frequency: ");
+			
+			try
+			{
+				res = Integer.parseInt(user.nextLine());
+			}
+			catch (NumberFormatException e)
+			{
+				System.out.println("ERROR! Compounding frequency must match menu selections!");
+				continue;
+			}
+			
+			if (res < 1 || res > 3)
+			{
+				System.out.println("ERROR! Compounding frequency must match menu selections!");
+				continue;
+			}
+		}
+		while (res < 1 || res > 3);
+		
+		return res;
+	}
+	
 }
